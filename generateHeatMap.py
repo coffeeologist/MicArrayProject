186ccf488056f8235d067440e4f11aca49e2a63b
coffeeologist <liuamy000@gmail.com>
2019-10-18 12:32:17 -0400

    initial attempt at extract matrix with Python

---
+++ generateHeatMap.py
@@ -0,0 +1,45 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+
+import os
+import subprocess
+import sys
+
+
+def generateMatrixFromWav(path):
+	if (os.path.isdir(path) == False):
+		if path.endswith(".wav"):
+            # convert into csv, maybe create a new folder?
+			blehstringCommand="ffprobe -f lavfi -i amovie="+path+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.Peak_level, -of csv=p=0 -print_format csv>"+path[:-4]+".csv"
+
+			stringCommand="ffprobe -f lavfi -i amovie="+path+",astats=metadata=1:reset=1 -show_entries frame=pkt_pts_time:frame_tags=lavfi.astats.Overall.Peak_level,lavfi.astats.Overall.RMS_peak,lavfi.astats.Overall.Flat_factor,lavfi.astats.Overall.Peak_count,lavfi.astats.Overall.Dynamic_range, -of csv=p=0 -print_format csv>"+path[:-4]+".csv"
+
+			subprocess.call(stringCommand, shell=True)
+
+            # call rscript to combine the csvs into a matrix
+
+            # delete all the extra csv
+
+	else:
+		for filename in os.listdir(path):
+			generateMatrixFromWav(path + "/" + filename)
+
+# To be written after we get a proper format csv data file
+# def generateHeatMap():
+#     # Compute x^2 + y^2 across a 2D grid
+#     x, y = np.meshgrid(range(-5, 5), range(-5, 5))
+#     z = x ** 2 + y ** 2
+
+#     # Convert this grid to columnar data expected by Altair
+#     source = pd.DataFrame({'x': x.ravel(),
+#                         'y': y.ravel(),
+#                         'z': z.ravel()})
+
+#     alt.Chart(source).mark_rect().encode(
+#         x='x:O',
+#         y='y:O',
+#         color='z:Q'
+#     ).save('chart.jpg')
+
+generateMatrixFromWav(sys.argv[1])
