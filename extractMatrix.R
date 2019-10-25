67ca5d6c8ac59f61f13b64ec769ea33f132db2f3
coffeeologist <liuamy000@gmail.com>
2019-10-25 09:34:33 -0400

    added correct labels, improved code readability

--- extractMatrix.R
+++ extractMatrix.R
@@ -7,16 +7,13 @@ RMS <- function(num) {sqrt(sum(num^2)/length(num))}
 extractMatrix <- function (fileRepo, intervalLen, verbose=FALSE, showWarnings=TRUE) {
   files <- list.files(path=fileRepo, pattern="*.wav", full.names=TRUE, recursive=TRUE, include.dirs = TRUE)
   
-  # max <- 0
-  # for (file in files){
-  #   sound = readWave(file)
-  #   length = length(sound)
-  #   if(!is.null(length) && length > max){ max <- length }  }
+  # Figure out duration & calculate length of the resultin gmatrix based on the interval length
   firstOne <- readWave(files[[1]])
   max <- length(firstOne) / firstOne@samp.rate 
+  resLen <- max/intervalLen
   print(max)
   print(intervalLen)
-  resLen <- max/intervalLen 
+  print(resLen) 
 
   mRes <- matrix(nrow = 12, ncol = resLen)
   rowCounter = 1
@@ -25,48 +22,37 @@ extractMatrix <- function (fileRepo, intervalLen, verbose=FALSE, showWarnings=TR
     # read in audio file
     data = readWave(fin)
     
-    # extract signal
-    # length is 2,880,000
+    # extract signal # length is 2,880,000
     snd = data@left
 
+    # take RMS over an interval
     factor <- length(snd)/resLen
-    
-    # cut down the amount of samples
     intervaledSnd = resLen
     for(i in 1:resLen){
       end = 0
       if(factor*(i+1) > length(snd)){end = length(snd)-1}else{end = factor*(i+1)}
       range <- snd[(factor*i) : end]
-      # print("now considering the subvectors from ")
-      # print(factor*i)
-      # print("to")
-      # print(end)
       intervaledSnd[[i]] <- RMS(range)
-      #print(intervaledSnd[[i]])
     }
 
     # print(snd)
     mRes[rowCounter, ] <- intervaledSnd
     
-    timeArray <- (1:resLen) * intervalLen
-    #timeArray <- ((0:(length(intervaledSnd)-1))) / (data@samp.rate) * 60
-
     rowCounter = rowCounter + 1
     
   }
   
-  
+  # add labels to the matrix
   rownames(mRes) <- c("Mic1", "Mic2", "Mic3", "Mic4","Mic5", "Mic6", "Mic7", "Mic8","Mic9", "Mic10","Mic11", "Mic12")
-  colnames(mRes) <- timeArray
+  colnames(mRes) <- (1:resLen) * intervalLen
+  print((1:resLen)*intervalLen)
   
+  # transpose & output
   mRes <- t(mRes)
-  print(length(mRes))
-  #colnames(mRes) <- c("Mic1", "Mic2", "Mic3", "Mic4","Mic5", "Mic6", "Mic7", "Mic8","Mic9", "Mic10","Mic11", "Mic12")
-  #rownames(mRes) <- timeArray
-  
   write.csv(mRes,file="data.csv")
 
+  print("Finished.")
 }
   
-# Collect csvFiles
+if(length(args) < 2) {print("Please input an interval length (as a arabic number).")}
 extractMatrix(args[1], as.numeric(args[2]))
