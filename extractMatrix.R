089ba0c00d8a4ebedafaf91e7447a2f055a67961
coffeeologist <liuamy000@gmail.com>
2019-10-25 09:20:44 -0400

    Fix the disappearance of the last row in csvs

--- extractMatrix.R
+++ extractMatrix.R
@@ -1,19 +1,24 @@
 args = commandArgs(trailingOnly=TRUE)
-library(tuneR, ggplot2, warn.conflicts = F, quietly = T) 
+library(tuneR, warn.conflicts = F, quietly = T) 
 
 RMS <- function(num) {sqrt(sum(num^2)/length(num))}
 
 # Assumes 12 channels (ie .wav files) in the fileRepo
-extractMatrix <- function (fileRepo, verbose=FALSE, showWarnings=TRUE) {
+extractMatrix <- function (fileRepo, intervalLen, verbose=FALSE, showWarnings=TRUE) {
   files <- list.files(path=fileRepo, pattern="*.wav", full.names=TRUE, recursive=TRUE, include.dirs = TRUE)
   
-  max <- 0
-  for (file in files){
-    sound = readWave(file)
-    length = length(sound)
-    if(!is.null(length) && length > max){ max <- length }  }
-  
-  mRes <- matrix(nrow = 12, ncol = 1200)
+  # max <- 0
+  # for (file in files){
+  #   sound = readWave(file)
+  #   length = length(sound)
+  #   if(!is.null(length) && length > max){ max <- length }  }
+  firstOne <- readWave(files[[1]])
+  max <- length(firstOne) / firstOne@samp.rate 
+  print(max)
+  print(intervalLen)
+  resLen <- max/intervalLen 
+
+  mRes <- matrix(nrow = 12, ncol = resLen)
   rowCounter = 1
   
   for(fin in files){
@@ -24,11 +29,11 @@ extractMatrix <- function (fileRepo, verbose=FALSE, showWarnings=TRUE) {
     # length is 2,880,000
     snd = data@left
 
-    factor <- length(snd)/1200
+    factor <- length(snd)/resLen
     
     # cut down the amount of samples
-    intervaledSnd = 1200
-    for(i in 1:1200){
+    intervaledSnd = resLen
+    for(i in 1:resLen){
       end = 0
       if(factor*(i+1) > length(snd)){end = length(snd)-1}else{end = factor*(i+1)}
       range <- snd[(factor*i) : end]
@@ -43,7 +48,7 @@ extractMatrix <- function (fileRepo, verbose=FALSE, showWarnings=TRUE) {
     # print(snd)
     mRes[rowCounter, ] <- intervaledSnd
     
-    timeArray <- (0:1200)
+    timeArray <- (1:resLen) * intervalLen
     #timeArray <- ((0:(length(intervaledSnd)-1))) / (data@samp.rate) * 60
 
     rowCounter = rowCounter + 1
@@ -51,6 +56,8 @@ extractMatrix <- function (fileRepo, verbose=FALSE, showWarnings=TRUE) {
   }
   
   
+  rownames(mRes) <- c("Mic1", "Mic2", "Mic3", "Mic4","Mic5", "Mic6", "Mic7", "Mic8","Mic9", "Mic10","Mic11", "Mic12")
+  colnames(mRes) <- timeArray
   
   mRes <- t(mRes)
   print(length(mRes))
@@ -60,12 +67,6 @@ extractMatrix <- function (fileRepo, verbose=FALSE, showWarnings=TRUE) {
   write.csv(mRes,file="data.csv")
 
 }
-
-
-# This function finds the longest duration given a list of (wav) files.
-findMaxDuration <- function(fileList, verbose=FALSE, showWarnings=TRUE) {
-  
-}
   
 # Collect csvFiles
-extractMatrix(args[1])
+extractMatrix(args[1], as.numeric(args[2]))
