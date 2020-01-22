aa1ad4e367ac29d30f07769c46e871cd380eee0d
Allen Liu <allenliu225@gmail.com>
2020-01-22 11:16:07 -0500

    fixed logistic regression script

--- classification/LogisticRegression.py
+++ classification/LogisticRegression.py
@@ -5,11 +5,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 def classify(df):
-    df = df.query('Label == "S" or Label == "H"')
+    print(df)
+    df = df.query('Label in ["H","S"]')
+    print(df)
 
     X = df[["Peak Level", "Dynamic Range", "RMS Level", "RMS Variance"]].values
     y = df["Label"].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, train_size=0.5)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, train_size=0.75)
 
     lr = LogisticRegression()
     lr.fit(X_train, y_train)
@@ -20,6 +22,5 @@ def classify(df):
 # sys.argv[1] - .csv file with all required data (export from librosaScript)
 data = data = pd.read_csv(open(sys.argv[1], "r"),
                    sep = ',', 
-                   names=["Start", "End", "Peak Level", "Dynamic Range", "RMS Level", "RMS Variance", "Label"],
-                   index_col=False)
+                   names=["Start", "End", "Peak Level", "Dynamic Range", "RMS Level", "RMS Variance", "Label"])
 classify(data)
