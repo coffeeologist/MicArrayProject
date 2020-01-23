67365277cbe6d43f9d395e715bdd6667a4bd3852
coffeeologist <liuamy000@gmail.com>
2020-01-23 16:24:03 -0500

    Added graphs of the classification features

--- classification/graphDataCSV.py
+++ classification/graphDataCSV.py
@@ -6,9 +6,14 @@ def graph(data):
 
     source = pd.read_csv(data)
 
-    alt.Chart(source).mark_circle(size=5).encode(
-        x='Start',
-        y=alt.Y('RMS Variance', scale=alt.Scale(domain=[0.000, 0.000020])),
+    '''
+    toDrop = source[ (source['Label'] != 'S')].index
+    source.drop(toDrop , inplace=True)
+    
+    '''
+    alt.Chart(source).mark_bar().encode(
+        x=alt.X('Dynamic Range:Q', bin=alt.Bin(extent=[0,5], step=0.05)),
+        y='count()',
         color='Label:N'
     ).save('data_csv_graphed.png', scale_factor=2.0)
 
